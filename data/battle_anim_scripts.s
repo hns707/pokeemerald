396aa0ce99b30dc802d043ad799e5920cecab75c
HNS <72392569+hns707@users.noreply.github.com>
2023-09-10 10:28:44 +0200

    move edit

--- data/battle_anim_scripts.s
+++ data/battle_anim_scripts.s
@@ -6391,38 +6391,14 @@ WhirlpoolEffect:
 	return
 
 Move_PUNISH_BLAST:
-	loadspritegfx ANIM_TAG_ELECTRIC_ORBS
-	loadspritegfx ANIM_TAG_CIRCLE_OF_LIGHT
-	loadspritegfx ANIM_TAG_SPARK
-	monbg ANIM_ATTACKER
-	setalpha 12, 8
-	createsprite gSimplePaletteBlendSpriteTemplate, ANIM_ATTACKER, 2, F_PAL_BG, 2, 0, 4, RGB_BLACK
-	waitforvisualfinish
-	createvisualtask AnimTask_ElectricChargingParticles, 2, ANIM_ATTACKER, 20, 0, 2
-	playsewithpan SE_M_CHARGE, SOUND_PAN_ATTACKER
-	delay 6
-	createsprite gGrowingShockWaveOrbSpriteTemplate, ANIM_ATTACKER, 2
-	delay 15
 	loadspritegfx ANIM_TAG_GOLD_RING
 	playsewithpan SE_M_PSYBEAM, SOUND_PAN_ATTACKER
+	call SetPsychicBackground
 	createsoundtask SoundTask_LoopSEAdjustPanning, SE_M_PSYBEAM2, SOUND_PAN_ATTACKER, SOUND_PAN_TARGET, 3, 4, 0, 15
 	call PsybeamRings
-	delay 15
 	call PsybeamRings
-	delay 12
-	call PsybeamRings
-	delay 9
-	call PsybeamRings
-	delay 5
-	call SetSolarBeamBg
 	createvisualtask AnimTask_SwayMon, 5, 0, 6, 2048, 4, ANIM_TARGET
-	createvisualtask AnimTask_BlendColorCycle, 2, F_PAL_TARGET, 2, 2, 0, 12, RGB_WHITE
-	call PsybeamRings
-	call PsybeamRings
-	call PsybeamRings
-	call PsybeamRings
-	call PsybeamRings
-	call PsybeamRings
+	createvisualtask AnimTask_BlendColorCycle, 2, F_PAL_TARGET, 2, 2, 0, 12, RGB(31, 18, 31)
 	call PsybeamRings
 	call PsybeamRings
 	call PsybeamRings
@@ -6434,7 +6410,7 @@ Move_PUNISH_BLAST:
 	call PsybeamRings
 	waitforvisualfinish
 	delay 1
-	call UnsetSolarBeamBg
+	call UnsetPsychicBackground
 	end
 
 Move_FLY:
